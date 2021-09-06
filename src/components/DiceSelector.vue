c053863edda674e527f1e12c4cea3874979fd01b
tilsche <thomas.ilsche@tu-dresden.de>
2021-09-06 21:56:53 +0200

    stuff

---
+++ src/components/DiceSelector.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li v-for="die in dice" :key="die">
+      {{ die }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'DiceSelector',
+  data () {
+    return {
+      dice: [this.app.config.globalProperties.dice.attack,
+        ...this.app.config.globalProperties.dice.power,
+        ...this.app.config.globalProperties.dice.defense]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
