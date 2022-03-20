5366e442ab6d65c7664703af270fb6d9d8beec74
zulan <git@zulan.net>
2022-03-20 18:30:10 +0100

    fun with dice

--- src/components/DiceSelector.vue
+++ src/components/DiceSelector.vue
@@ -1,30 +1,48 @@
 <template>
-  <ul>
-    <li v-for="die in dice" :key="die">
-      <img :src="die.image">
-    </li>
-  </ul>
+  <div class="container">
+    <ul class="list-inline">
+      <li v-for="die in available" :key="die" class="list-inline-item">
+        <img :src="die.image" class="dice" @click="add(die)">
+      </li>
+    </ul>
+  </div>
+  <div class="container">
+    <ul class="list-inline">
+      <li v-for="die in selected" :key="die" class="list-inline-item">
+        <img :src="die.image" class="dice">
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { CombatDie } from '@/dice'
 // import { CombatDie, DefenseDie } from '@/dice'
 
 export default defineComponent({
   name: 'DiceSelector',
   data () {
     return {
-      dice: [this.$dice.attack,
+      available: [this.$dice.attack,
         ...this.$dice.power,
-        ...this.$dice.defense]
+        ...this.$dice.defense],
+      selected: [this.$dice.attack]
     }
   },
   computed: {
+  },
+  methods: {
+    add (die: CombatDie) {
+      this.selected.push(die)
+    }
   }
 })
 
 </script>
 
 <style scoped>
-
+.dice {
+  width: 4rem
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import { CombatDie, DefenseDie } from './dice'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
