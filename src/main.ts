effd9370269b6cc89b1b0d02b63323d1da9c6e17
zulan <git@zulan.net>
2022-03-20 18:32:37 +0100

    replace vuex with pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import store from './store'
 import { CombatDie, DefenseDie } from './dice'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
