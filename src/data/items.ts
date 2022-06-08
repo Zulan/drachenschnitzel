3f1bbf492b9f821bcc49f8dbb872030e377c09d5
zulan <git@zulan.net>
2022-06-08 23:33:18 +0200

    fix ts

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,7 +1,8 @@
 import { isWeapon, parseShopWeapon } from "@/models/items";
 import type { Weapon } from "@/models/items";
 import jsonShopItems from "@/data/d2e/shop-items.json";
+import { combatDiceByColor } from "@/data/dicePool";
 
 export const weapons: Weapon[] = jsonShopItems
   .filter(isWeapon)
-  .map(parseShopWeapon);
+  .map((weapon) => parseShopWeapon(weapon, combatDiceByColor));
