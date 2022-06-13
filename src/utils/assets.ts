6d1127e321bb7d18a4150f7449209769b28df84b
zulan <git@zulan.net>
2022-06-13 20:16:34 +0200

    improve assetUrl

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -1,3 +1,11 @@
+const assets = import.meta.globEager("@/assets/**/*", { as: "url" });
+
 export function assetUrl(path: string): string {
-  return new URL(`../assets/${path}`, import.meta.url).href;
+  const key = Object.keys(assets).find((fullPath) =>
+    fullPath.endsWith(`/assets/${path}`)
+  );
+  if (key === undefined) {
+    throw Error(`asset "${path}" not found`);
+  }
+  return assets[key].default;
 }
