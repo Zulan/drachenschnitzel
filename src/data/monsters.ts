a6b00a269bc2ea1a7cb788c8fe5dea4504a36917
zulan <git@zulan.net>
2022-06-26 10:57:11 +0200

    skip conversion kit monsters

--- src/data/monsters.ts
+++ src/data/monsters.ts
@@ -3,10 +3,12 @@ import { Monster } from "@/models/monster";
 import jsonMonsters from "@/data/d2e/monsters.json";
 import { pairwise } from "@/utils/pairwise";
 
-export const monsters: Monster[] = Array.from(pairwise(jsonMonsters)).map(
-  ([jsonFront, jsonBack]) =>
-    new Monster(
-      jsonFront as unknown as JsonFront,
-      jsonBack as unknown as JsonBack
-    )
-);
+export const monsters: Monster[] = Array.from(pairwise(jsonMonsters))
+  .map(
+    ([jsonFront, jsonBack]) =>
+      new Monster(
+        jsonFront as unknown as JsonFront,
+        jsonBack as unknown as JsonBack
+      )
+  )
+  .filter((monster) => monster.expansion !== "Conversion Kit");
